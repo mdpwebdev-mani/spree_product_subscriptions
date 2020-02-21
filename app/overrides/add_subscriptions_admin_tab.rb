2d057792e15b8ea909743a55905d4003eff86879
Ricardo Mazzini <mazzini@bit32.it>
2020-02-21 15:28:12 -0500

    layout

--- app/overrides/add_subscriptions_admin_tab.rb
+++ app/overrides/add_subscriptions_admin_tab.rb
@@ -1,6 +1,15 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'subscriptions_admin_tab',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/subscriptions_sidebar_menu'
-)
+if Spree.version.to_f < 4.0
+  Deface::Override.new(
+    virtual_path: 'spree/layouts/admin',
+    name: 'subscriptions_admin_tab',
+    insert_bottom: '#main-sidebar',
+    partial: 'spree/admin/shared/subscriptions_sidebar_menu'
+  )
+else
+  Deface::Override.new(
+    virtual_path: 'spree/admin/shared/_main_menu',
+    name: 'subscriptions_admin_tab',
+    insert_bottom: 'nav',
+    partial: 'spree/admin/shared/subscriptions_sidebar_menu'
+  )
+end
